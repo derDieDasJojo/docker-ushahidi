0e111950f41e7f745557452994c915eae6264538
Jojo <git config --global user.name Your>
2016-07-28 15:11:37 +0200

    added ushahidi platform src

---
+++ platform/src/application/classes/Ushahidi/Formatter/Post/GeoJSONCollection.php
@@ -0,0 +1,103 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Ushahidi API Formatter for Post
+ *
+ * @author     Ushahidi Team <team@ushahidi.com>
+ * @package    Ushahidi\Application
+ * @copyright  2014 Ushahidi
+ * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
+ */
+
+use Ushahidi\Core\Tool\Formatter;
+use Ushahidi\Core\Tool\Formatter\GeoJSONFormatter;
+use Ushahidi\Core\SearchData;
+
+class Ushahidi_Formatter_Post_GeoJSONCollection implements Formatter
+{
+	use GeoJSONFormatter;
+
+	// Formatter
+	public function __invoke($entities)
+	{
+		if (!is_array($entities)) {
+			throw new FormatterException('Collection formatter requries an array of entities');
+		}
+
+		$output = [
+			'type' => 'FeatureCollection',
+			'features' => []
+		];
+
+		foreach ($entities as $entity)
+		{
+			$geometries = [];
+			foreach($entity->values as $attribute => $values)
+			{
+				foreach ($values as $value)
+				{
+					if ($geometry = $this->valueToGeometry($value))
+					{
+						$geometries[] = $geometry;
+					}
+				}
+			}
+
+			if (! empty($geometries))
+			{
+				$color = ltrim($entity->color, '#');
+				$color = $color ? '#' . $color : null;
+				
+				$output['features'][] = [
+					'type' => 'Feature',
+					'geometry' => [
+						'type' => 'GeometryCollection',
+						'geometries' => $geometries
+					],
+					'properties' => [
+						'title' => $entity->title,
+						'description' => $entity->content,
+						'marker-color' => $color,
+						'id' => $entity->id,
+						'url' => URL::site(Ushahidi_Rest::url($entity->getResource(), $entity->id), Request::current()),
+						// @todo add mark- attributes based on tag symbol+color
+						//'marker-size' => '',
+						//'marker-symbol' => '',
+						//'resource' => $entity
+					]
+				];
+			}
+		}
+
+		if ($this->search->bbox)
+		{
+			if (is_array($this->search->bbox))
+			{
+				$bbox = $this->search->bbox;
+			}
+			else
+			{
+				$bbox = explode(',', $this->search->bbox);
+			}
+
+			$output['bbox'] = $bbox;
+		}
+
+		return $output;
+	}
+
+	/**
+	 * Store paging parameters.
+	 *
+	 * @param  SearchData $search
+	 * @param  Integer    $total
+	 * @return $this
+	 */
+	public function setSearch(SearchData $search, $total = null)
+	{
+		$this->search = $search;
+		$this->total  = $total;
+		return $this;
+	}
+
+}
---
+++ platform/src/docker/test.run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+check_vols_src() {
+  if [ ! -d /vols/src ]; then
+    echo "No /vols/src with code"
+    exit 1
+  fi
+}
+
+function sync {
+  check_vols_src
+  {
+    for f in bin/*; do
+      echo "- ${f}"
+    done
+    for f in modules/*; do
+      echo "- ${f}"
+    done
+    echo "- .git"
+    echo "- vendor"
+    echo "- tmp"
+  } > /tmp/rsync_exclude
+  rsync -ar --exclude-from=/tmp/rsync_exclude --delete-during /vols/src/ ./
+  rm -f phpunit.xml behat.yml phpspec.yml
+}
+
+function run_composer_install {
+  composer install --no-interaction
+}
+
+function wait_for_mysql {
+  until nc -z mysql 3306; do
+    >&2 echo "Mysql is unavailable - sleeping"
+    sleep 1
+  done
+}
+
+sync
+run_composer_install
+cp .env.testing .env
+wait_for_mysql
+bin/phinx migrate -c application/phinx.php
+php -S localhost:8000 -t httpdocs httpdocs/index.php &
+
+exec $*
